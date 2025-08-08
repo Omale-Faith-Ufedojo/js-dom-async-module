407a691c78e5bf3fb4cf9948249a6c139557ff6f
Omale Faith Ufedo-Ojo <omalefaithufedojo@gmail.com>
2025-08-08 18:31:40 +0100

    Add fetchDataCallback assignment

---
+++ tasks/callback-assignment/fetchDataWithCallback.js
@@ -0,0 +1,17 @@
+function fetchDataWithCallback(callback) {
+  setTimeout(function () {
+    let error = true; 
+      try {
+      if (error) {
+        throw new Error("Fetch failed");
+      }
+      callback("Data fetched");
+    } catch (err) {
+      console.log("Error: Fetch failed");
+    }
+  }, 2000);
+}
+
+fetchDataWithCallback(function (message) {
+  console.log(message);
+});
